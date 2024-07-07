638d2a1c7fe13061961a7a047a23d171a8a8d76e
Jay Rajendra Gite <jaygite544@gmail.com>
2024-07-07 17:32:21 +0530

    Leetcode Water Bottles solution

---
+++ solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+    int ans=0;
+        if(numBottles < numExchange){
+             return numBottles;
+        }
+        if(numBottles == 0){
+            return 0;
+        }
+    int carry=0;
+        while(numBottles != 0){  
+            ans += numBottles;
+            numBottles += carry;
+            
+            carry = numBottles % numExchange;
+            numBottles /= numExchange;
+        }
+        return ans;
+    }
+};  
